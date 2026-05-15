0792dc5088dc8fddc71eebd15c6f7a5b7e715a4a
cllsm-314 <zhangchunlei@delonixmail.com>
2026-05-15 09:08:38 +0800

    Update ChatBox.tsx

--- src/components/ChatBox.tsx
+++ src/components/ChatBox.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import Image from 'next/image'
 import { useState, useEffect, useRef, useCallback } from 'react'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
